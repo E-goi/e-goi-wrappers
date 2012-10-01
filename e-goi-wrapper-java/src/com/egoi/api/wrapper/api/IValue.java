60cb8267144584f76915fa3ccf67966be211f397
Luís Lobo <ellobo@ipb.pt>
2012-10-01 23:38:42 +0100

    - IResult.Util class no longer needed - IValue class no longer needed

--- e-goi-wrapper-java/src/com/egoi/api/wrapper/api/IValue.java
+++
@@ -1,5 +0,0 @@
-package com.egoi.api.wrapper.api;
-
-public interface IValue {
-
-}
--- e-goi-wrapper-java/src/com/egoi/api/wrapper/api/MapResultList.java
+++ e-goi-wrapper-java/src/com/egoi/api/wrapper/api/MapResultList.java
@@ -3,6 +3,6 @@ package com.egoi.api.wrapper.api;
 import java.util.ArrayList;
 
 @SuppressWarnings("serial")
-public class MapResultList extends ArrayList<IResult> implements IResult, IValue {
+public class MapResultList extends ArrayList<IResult> implements IResult {
 
 }
