26df35ed14a54c2534330f18f958d106dae94790
Luís Lobo <ellobo@ipb.pt>
2012-12-08 11:35:40 +0000

    - C# XmlRpc complete

---
+++ e-goi-wrapper-csharp/Test/Example.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Egoi;
+using CookComputing.XmlRpc;
+
+namespace Test
+{
+    class Example
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                EgoiMap arguments = new EgoiMap();
+                arguments.Add("apikey", "<api key>");
+                EgoiApi api = EgoiApiFactory.getApi();
+                
+                // Obter os dados do utilizador da ApiKey
+                EgoiMap userData = api.getUserData(arguments);
+                foreach(string key in userData.Keys) {
+                    Console.WriteLine(key + "=" + userData[key]);
+                }
+
+                // Obter as listas
+                EgoiMapList lists = api.getLists(arguments);
+                foreach (EgoiMap list in lists) {
+                    // Os tipos EgoiMap/EgoiMapList devolvem um output parecido com
+                    // JSON no seu ToString()
+                    Console.WriteLine(list);
+                }
+
+            }
+            catch (EgoiException e)
+            {
+                Console.WriteLine("Ocorreu um erro: " + e.Message);
+            }
+
+            // Esperar por um enter na consola
+            Console.Read();
+        }
+    }
+}
