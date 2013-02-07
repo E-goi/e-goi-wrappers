6493113ea2b507ea2b15c6175689644d06a29f82
Luís Lobo <ellobo@ipb.pt>
2013-02-07 23:39:36 +0000

    - Solved ISSUE-4

--- e-goi-wrapper-csharp/SampleProject/EgoiApiSample/MainForm.cs
+++ e-goi-wrapper-csharp/SampleProject/EgoiApiSample/MainForm.cs
@@ -28,48 +28,17 @@ namespace EgoiSample
         {
             status.Text = "";
             output.Clear();
-            Protocol p = (Protocol)protocol.SelectedItem;
+            Protocol p = (Protocol) protocol.SelectedItem;
             try
             {
-                EgoiApi api = EgoiApiFactory.getApi(Egoi.Protocol.XmlRpc);
-
+                EgoiApi api = EgoiApiFactory.getApi(p);
                 EgoiMap arguments = new EgoiMap();
-
-                String apikey = "d7cdcc90c0547da5da90c3e14eeff180455c38a2";
-                String listId = "2";
-
-                // Struct do Subscriber 1
-                EgoiMap subscriber1 = new EgoiMap();
-                subscriber1.Add("email", "joaodelfino@gmail.com");
-                subscriber1.Add("status", "4");
-
-                // Struct do Subscriber 2
-                EgoiMap subscriber2 = new EgoiMap();
-                subscriber2.Add("email", "joao.delfino@hotmail.co.uk");
-                subscriber2.Add("status", "4");
-
-                // Struct do Subscriber 1
-                EgoiMap subscriber3 = new EgoiMap();
-                subscriber3.Add("email", "jp.delfino@clix.pt");
-                subscriber3.Add("status", "0");
-
-                // Lista de Subscribers
-                EgoiMapList subscribers = new EgoiMapList();
-                subscribers.Add(subscriber1);
-                subscribers.Add(subscriber2);
-                subscribers.Add(subscriber3);
-
-                // Struct com os argumentos
-                arguments.Add("apikey", apikey);
-                arguments.Add("listID", listId);
-                arguments.Add("compareField", "email");
-                arguments.Add("subscribers", subscribers); // lista de subscribers
-
-                EgoiMap result = api.addSubscriberBulk(arguments);
+                arguments.Add("apikey", apiKey.Text);
+                EgoiMap result = api.getUserData(arguments);
                 output.Text = result.ToString();
             } catch(EgoiException ex) {
                 status.Text = ex.Message;
-            }
+            }   
         }
     }
 }
